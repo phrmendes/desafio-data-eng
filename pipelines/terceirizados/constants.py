4cdf1d44feb0dc790237b9b98225ff97ba62c5d6
Pedro Mendes <pedrohrmendes@proton.me>
2024-07-25 22:39:16 -0300

    feat(pipelines): add data cleaning module

---
+++ pipelines/terceirizados/constants.py
@@ -0,0 +1,31 @@
+DOWNLOAD_LINK_CLASS = "cover-richtext-tile tile-content"
+REQUEST_TIMEOUT = 10
+NA_VALUES = ["NI  ", "<N/I>", "-2"]
+DATE_COLUMNS = ["num_mes_carga", "ano_carga", "mes_carga"]
+INT_COLUMNS = ["nr_cnpj"]
+NUMERIC_COLUMNS = [*INT_COLUMNS, "vl_mensal_salario", "vl_mensal_custo", "nr_jornada"]
+COLUMNS = [
+    "id_terc",
+    "sg_orgao_sup_tabela_ug",
+    "cd_ug_gestora",
+    "nm_ug_tabela_ug",
+    "sg_ug_gestora",
+    "nr_contrato",
+    "nr_cnpj",
+    "nm_razao_social",
+    "nr_cpf",
+    "nm_terceirizado",
+    "nm_categoria_profissional",
+    "nm_escolaridade",
+    "nr_jornada",
+    "nm_unidade_prestacao",
+    "vl_mensal_salario",
+    "vl_mensal_custo",
+    "Num_Mes_Carga",
+    "Mes_Carga",
+    "Ano_Carga",
+    "sg_orgao",
+    "nm_orgao",
+    "cd_orgao_siafi",
+    "cd_orgao_siape",
+]
---
+++ pipelines/terceirizados/data.py
@@ -0,0 +1,147 @@
+import gc
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+from pathlib import Path
+
+import pandas as pd
+from bs4 import BeautifulSoup
+from janitor import clean_names, remove_empty
+from loguru import logger
+from requests import get
+
+import pipelines.terceirizados.constants as const
+from pipelines.settings import Settings
+
+read_csv = partial(
+    pd.read_csv,
+    sep=";",
+    na_values=const.NA_VALUES,
+    dtype=object,
+)
+
+read_excel = partial(
+    pd.read_excel,
+    na_values=const.NA_VALUES,
+    dtype=object,
+)
+
+
+def get_date_extension(url: str) -> tuple[str, str]:
+    """Get the date and the extension from the file in the URL."""
+    file_name = url.split("/")[-1]
+    name, extension = file_name.split(".")
+
+    date = (
+        name.removeprefix("terceirizados")
+        .removeprefix("-")
+        .removeprefix("_")
+        .removesuffix("_1")
+    )
+
+    return date, extension
+
+
+def add_date_column(data: pd.DataFrame) -> pd.DataFrame:
+    """Create a date column in the YYYY-MM-DD format."""
+    month = const.DATE_COLUMNS[0]
+    year = const.DATE_COLUMNS[1]
+    date = pd.to_datetime(data[year].astype(str) + "-" + data[month].astype(str))
+    data = data.assign(date=date)
+
+    return data.drop(const.DATE_COLUMNS, axis=1)
+
+
+def check_header(data: pd.DataFrame) -> pd.DataFrame:
+    """Add a header to the data.
+
+    It checks if the header is correct. If it is not, it will get
+    the first line of the data and consider as the first line of
+    the dataframe and add the correct header.
+    """
+    first_line = list(data.columns)
+    header = list(const.COLUMNS)
+
+    if first_line != header:
+        first_line = pd.DataFrame(first_line).transpose()
+
+        for df in [first_line, data]:
+            df.columns = header
+
+        return pd.concat([first_line, data], ignore_index=True)
+
+    return data
+
+
+def handle_numeric_columns(data: pd.DataFrame) -> pd.DataFrame:
+    """Handle numeric columns with special characters."""
+    for column in const.NUMERIC_COLUMNS:
+        data[column] = (
+            data[column]
+            .astype(str)
+            .str.replace(",", ".")
+            .str.replace("_", "")
+            .astype(float)
+        )
+
+    for column in const.INT_COLUMNS:
+        data[column] = data[column].astype(int)
+
+    return data
+
+
+@logger.catch
+def clear(data: pd.DataFrame) -> pd.DataFrame:
+    """Aggregate all cleaning steps."""
+    return (
+        data.pipe(remove_empty)
+        .pipe(check_header)
+        .pipe(clean_names)
+        .pipe(handle_numeric_columns)
+        .pipe(add_date_column)
+    )
+
+
+@logger.catch
+def download(url: str) -> None:
+    """Download a spreadsheet."""
+    date, ext = get_date_extension(url)
+    year = date[:4]
+    month = date[4:]
+
+    tempfile = Path(f"output/{date}.csv")
+    tempfile.parent.mkdir(exist_ok=True)
+
+    logger.info(f"Download - year: {year}, month: {month}, extension: {ext}")
+
+    try:
+        data = read_csv(url) if ext == "csv" else read_excel(url)
+    except UnicodeDecodeError:
+        logger.error("Encoding error - using latin-1")
+        data = read_csv(url, encoding="latin-1")
+    finally:
+        logger.success("Download completed")
+
+    data.pipe(clear).to_csv(tempfile)
+
+    del data
+    gc.collect()
+
+
+def get_links(settings: Settings) -> list[str]:
+    """Get all spreadsheets from the website."""
+    response = get(settings.BASE_URL, timeout=const.REQUEST_TIMEOUT)
+    page = BeautifulSoup(response.text, "html.parser")
+    divs = page.find_all("div", {"class": const.DOWNLOAD_LINK_CLASS})
+
+    return [
+        anchor["href"]
+        for div in divs
+        for anchor in div.find_all("a")
+        if anchor["href"].endswith((".csv", ".xlsx"))
+    ]
+
+
+def save(urls: list[str], max_workers: int = 6) -> None:
+    """Save all spreadsheets in the local storage."""
+    with ThreadPoolExecutor(max_workers) as executor:
+        executor.map(download, urls)
